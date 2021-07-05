77cd96810ef10bddf46ddda7ff9b99b12641cbf1
Abdurrahman <abdurrahmanseyidoglu@gmail.com>
2021-07-05 22:44:42 +0300

    adding the navbar of the webpage

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -3,12 +3,15 @@
     <nav class="header__menu">
       <div class="heade__menu-left">
       <router-link to="/" class="header__link">Home</router-link>
-      <router-link to="/contact" class="header__link">Contact</router-link>
-      <router-link to="/about" class="header__link">About</router-link>
+      <router-link to="/movies" class="header__link">Movies</router-link>
+      <router-link to="/tvshows" class="header__link">Tv Shows</router-link>
       </div>
       <div class="heade__menue-right">
-      <input type="text" placeholder="Search...">
+      <input type="text" placeholder="Search..." class="header__search">
+      <button class="header__search-button">Search</button>
       <router-link to="/about" class="header__link">About</router-link>
+      <router-link to="/contact" class="header__link">Contact</router-link>
+
 
       </div>
     </nav>
@@ -22,10 +25,36 @@
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: space-between;
+    background-color: $black;
+    padding: 30px;
    
   }
   &__link{
-      font-size:1.5rem;
+      font-size:1.1rem;
+      margin :15px;
+      text-decoration: none;
+      color: white;
+      &:hover{
+        color: $red;
+      }
+  }
+  &__search{
+      font-size:1.1rem;
+      border:thin;
+      border-radius: $radius;
+      height: 30px;
+      
+  }
+  &__search-button{
+    margin: 10px;
+    font-size:1.1rem;
+    font-weight: bold;
+    border-radius: $radius;
+    &:hover{
+      color: $white;
+      background: $red;
+      
+    }
   }
   
 }
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeComponent from '../views/home/Home.vue';
 import ContactComponent from '../views/about/About.vue';
 import AboutComponent from '../views/about/About.vue';
+import LatestComponent from '../views/latest/Latest.vue';
+import MoviesComponent from '../views/movies/Movies.vue';
+import TvShowsComponent from '../views/tv-shows/TvShows.vue';
+
 
 
 const routes = [
@@ -19,6 +23,21 @@ const routes = [
     path: '/contact',
     name: 'Contact',
     component: ContactComponent
+  },
+  {
+    path: '/latest',
+    name: 'Latest',
+    component: LatestComponent
+  },
+  {
+    path: '/movies',
+    name: 'Movies',
+    component: MoviesComponent
+  },
+  {
+    path: '/tvshows',
+    name: 'TvShows',
+    component: TvShowsComponent
   },
 ];
 
