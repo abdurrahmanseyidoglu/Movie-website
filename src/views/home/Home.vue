bca5c74418f194741c1be305a21087b488e39d3f
Abdurrahman <abdurrahmanseyidoglu@gmail.com>
2021-07-08 17:30:38 +0300

    fixing movie__card style

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -12,14 +12,15 @@
 
       <div class="movie__image-container">
       <img class="movie__image" :src="movieList.Poster" alt="Movie cover">
+      <div class="movie__type">Type : {{ movieList.Type }}</div>
       </div>
       <div class="movie__info">
       <div class="movie__title">Name : {{ movieList.Title }}</div>
-      <div class="movie__type">Type : {{ movieList.Type }}</div>
+      
       <div class="movie__year">Year :{{ movieList.Year }}</div> 
       <div class="movie__detailes">
       <router-link :to="'/movie/:id' + movieList.imdbID" class="movie__link">
-      <button class="movie__detailes-btn" >View Detailes</button>
+      <button class="movie__detailes-btn" >Detailes</button>
       </router-link>
       </div>
       </div>
@@ -40,11 +41,12 @@ export default {
     const movie = ref([]);
     const searchAPI= ()=>{
       if(search.vlaue != ""){
-        fetch(`http://www.omdbapi.com/?apikey=a697e544&s=${search.value}`)
+        fetch(`http://www.omdbapi.com/?s=${search.value}&apikey=a697e544&s`)
         .then(response =>response.json())
         .then(data => {
           //Search is an object key from the API
-          movie.value = data.Search;
+          
+          movie.value = data.Search ;
           //reset search box
           search.value = "";
           console.log(movie.value);
@@ -71,21 +73,24 @@ export default {
 </script>
 
 <style lang="scss">
-
+ 
 .home{
   background-color: $black;
 &__search-form{
+  align-items: center;
+  justify-content: center;
   display: flex;
   flex-direction: row;
   }
   &__search{
       border-radius: $radius;
+      background: $black;
       font-size: 1.2rem;
       &[type="text"]{
         color: $red;
       }
       &::placeholder{
-        color: indianred;
+        color: $red;
       }
       
   }
@@ -104,34 +109,61 @@ export default {
       display: flex;
       flex-direction: row;
       flex-wrap: wrap;
-      margin: 10px 10px;
+      align-items: center;
+      justify-content: center;
     &__info{
       align-items: center;
       justify-content: center;
     }
     &__card{
-      border: $red solid 3px ;
+      border: $gray solid 3px ;
       border-radius: 10px;
       margin: 10px 10px;
       width: 300px;
       height: 530px;
+      &:hover{
+      transition :background-color 1s ease ;
+      background-color: $red;
+      box-shadow : 0px 8px 8px 0px $white ;
+   
+      }
+    }
+    &__info{
+      color: $white;
     }
     &__title{
-      color: $white;
-      word-break: break-all;
-    }
+      overflow: hidden;
+      white-space: nowrap;
+          }
     &__type{
       color: $white;
+      display: block;      
     }
     &__year{
-      color: $white;
+    }
+    &__detailes{
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
     &__detailes-btn{
       background-color: $black;
       border: $red 2px solid;
-      border-radius: 3px;
+      border-radius: 5px;
+      font-size: 1rem;
+      color: $white;
+      padding: 6px;
+      &:hover{
+        cursor: pointer;
+        transition :background-color 1s ease ;
+        background-color: $gray;
+        border: $white 2px solid;
+      }
+
     }
     &__image{
+      display: block;
+     border-radius: 4px;
       width: 294px;
       height: 400px;
       mask-type: cover;
